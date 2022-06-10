f9fd7b579ea8d78b8913bb883a7fcf9549642858
Qiang Zhou <zhouqiang.cl@gmail.com>
2022-06-10 12:28:47 +0800

    update asset
    Signed-off-by: Qiang Zhou <zhouqiang.cl@gmail.com>

--- asset/asset.go
+++ asset/asset.go
@@ -98,6 +98,52 @@ func GetAssetID(tokenType types.TokenType, address string, tokenQuantum *big.Int
 	return assetId
 }
 
+func GetAssetIDByAssetType(tokenType types.TokenType, assetType *big.Int, tokenId *big.Int) *big.Int {
+	assetId := assetType
+	switch tokenType {
+	case types.ERC721:
+		hash := solsha3.SoliditySHA3(
+			solsha3.String("NFT:"),
+			solsha3.Uint256(assetId.String()),
+			solsha3.Uint256(tokenId.String()),
+		)
+
+		assetId.SetString(hex.EncodeToString(hash), 16)
+		arg := new(big.Int)
+		arg.SetString("03FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF", 16)
+		assetId = assetId.And(assetId, arg)
+	case types.ERC20M, types.ERC721M:
+		blobHash := new(big.Int)
+		blobHash.SetString(hex.EncodeToString(solsha3.SoliditySHA3(
+			[]string{
+				"string",
+			},
+			[]interface{}{
+				"MINTABLE:", // TODO: support minting_blob
+			},
+		)), 16)
+		assetId.SetString(hex.EncodeToString(solsha3.SoliditySHA3(
+			[]string{
+				"string",
+				"uint256",
+				"uint256",
+			},
+			[]interface{}{
+				"MINTABLE:",
+				assetId,
+				blobHash,
+			},
+		)), 16)
+		arg := new(big.Int)
+		arg.SetString("0000FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF", 16)
+		arg1 := new(big.Int)
+		arg1.SetString("400000000000000000000000000000000000000000000000000000000000000", 16)
+		assetId = assetId.And(assetId, arg)
+		assetId = assetId.Or(assetId, arg1)
+	}
+	return assetId
+}
+
 func rightJust(s string, n int, fill string) string {
 	log.Debugf("rightJust: s: %s, n: %d, fill: %s", s, n, fill)
 	return strings.Repeat(fill, n-len(s)) + s
